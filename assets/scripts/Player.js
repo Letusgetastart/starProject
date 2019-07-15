f2217bd3618711d9abc1efd521fee189643f08ab
qqy <qqywork@163.com>
2019-07-15 09:59:58 +0800

    third update

---
+++ assets/scripts/Player.js
@@ -0,0 +1,61 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        //主角跳跃高度
+        jumpHeight:0,
+        //主角跳跃的持续时间
+        jumpDuration:0,
+        //最大移动速度
+        maxMoveSpeed:0,
+        //加速度
+        accel:0,
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+    },
+    setJumpAction:function(){
+        //跳跃上升
+        var jumpUp=cc.moveBy(this.jumpDuration,cc.v2(0,this.jumpHeight).easing(cc.easeCubicActionOut()));
+        //下落
+        var jumpDown=cc.moveBy(this.jumpDuration,cc.v2(0,-this.jumpHeight).easing(cc.easeCubicActionIn()));
+        //不断重复
+        return cc.repeatForever(cc.sequence(jumpUp,jumpDown));
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        // 初始化跳跃动作
+        this.jumpAction = this.setJumpAction();
+        this.node.runAction(this.jumpAction);
+    },
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
